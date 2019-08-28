36e5778a2e367c3a08694193b45ad31d3e5a8c91
zijie.jiang <zijie.jiang@thoughtworks.com>
2019-08-28 19:48:45 +0800

    2

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,7 +2,7 @@
 set -x
 set -e
 
-docker run --rm -v /tmp/gradle-caches:/root/.gradle/caches -v $WORKDIR/pipelines/$GO_PIPELINE_NAME:/home/gradle -w /home/gradle gradle:3.5-jdk8 gradle clean build
+docker run --rm -v /tmp/gradle-caches:/root/.gradle/caches -v $WORKDIR/pipelines/$GO_PIPELINE_NAME:/home/gradle -w /home/gradle gradle:4.4-jdk8 gradle clean build
 
 if [[ -z $DOCKER_REGISRTY ]]; then
   DOCKER_REGISRTY=10.29.5.155:5000
