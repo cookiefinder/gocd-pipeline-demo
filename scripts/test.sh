310519129f1338fd261659b15fe91ac25d0f086c
zijie.jiang <zijie.jiang@thoughtworks.com>
2019-08-28 19:44:02 +0800

    1

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,4 +2,4 @@
 set -x
 set -e
 
-docker run --rm -v /tmp/gradle-caches:/root/.gradle/caches -v /godata/pipelines/$GO_PIPELINE_NAME:/home/gradle -w /home/gradle gradle:3.5-jdk8 gradlew clean test
+docker run --rm -v /tmp/gradle-caches:/root/.gradle/caches -v $WORKDIR/pipelines/$GO_PIPELINE_NAME:/home/gradle -w /home/gradle gradle:3.5-jdk8 gradlew clean test
