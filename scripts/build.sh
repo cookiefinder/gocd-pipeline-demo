dea33a0ef4f353581bbb3ae50e77561796282dd2
zijie.jiang <zijie.jiang@thoughtworks.com>
2019-08-29 11:23:42 +0800

    3

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,7 +5,7 @@ set -e
 docker run --rm -v /tmp/gradle-caches:/home/gradle/.gradle/caches -v $WORKDIR/pipelines/$GO_PIPELINE_NAME:/home/gradle -w /home/gradle gradle:3.5-jdk8 ./gradlew clean build
 
 if [[ -z $DOCKER_REGISRTY ]]; then
-  DOCKER_REGISRTY=10.29.5.155:5000
+  DOCKER_REGISRTY=10.202.129.3:5000
 fi
 
 IMAGE_NAME=${DOCKER_REGISRTY}/pipeline-demo:${GO_PIPELINE_COUNTER}
