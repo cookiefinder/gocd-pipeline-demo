b36dd299e2f5d7efadd71e0a53ec40ea5833baa1
zijie.jiang <zijie.jiang@thoughtworks.com>
2019-09-03 14:23:31 +0800

    1

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,8 +2,8 @@ set -x
 set -e
 
 export RANCHER_URL=http://10.202.129.3:8080/v2-beta/projects/1a5
-export RANCHER_ACCESS_KEY=4FE2F16E90B6EED1A0AB
-export RANCHER_SECRET_KEY=2QizbCdBgqXbCN6BXPEqsB65LvnrxqEQoJaKoq8w
+export RANCHER_ACCESS_KEY=6D11CFEAD79F767181AB
+export RANCHER_SECRET_KEY=s1hNCydBHocCnyQksTR5EE2Xay4YSCCfCWRogj4B
 
 if [[ -z $DOCKER_REGISRTY ]]; then
   DOCKER_REGISRTY=10.202.129.3:5000
@@ -12,4 +12,4 @@ IMAGE_NAME=${DOCKER_REGISRTY}/pipeline-demo:${GO_PIPELINE_COUNTER}
 
 sed -i "s#<IMAGE_NAME>#$IMAGE_NAME#g" docker-compose.yml
 
-rancher-compose -p mst-user-service up -d -c --upgrade
+rancher-compose -p pipeline-demo up -d -c --upgrade
