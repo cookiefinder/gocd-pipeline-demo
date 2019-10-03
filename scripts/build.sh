72f897c3de890fd4435e1632b8891be5a5fad8b9
Zijie Jiang <zijie.jiang@thoughtworks.com>
2019-10-03 12:55:00 +0800

    [Zijie Jiang] refactor: fix test build and deploy script

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,10 +2,10 @@
 set -x
 set -e
 
-docker run --rm -v /tmp/gradle-caches:/home/gradle/.gradle/caches -v $WORKDIR/pipelines/$GO_PIPELINE_NAME:/home/gradle -w /home/gradle gradle:4.4-jdk8 gradle clean build
+cd /home/gradle && ./gradlew clean build
 
 if [[ -z $DOCKER_REGISRTY ]]; then
-  DOCKER_REGISRTY=10.202.129.3:5000
+  DOCKER_REGISRTY=10.10.2.156:5000
 fi
 
 IMAGE_NAME=${DOCKER_REGISRTY}/pipeline-demo:${GO_PIPELINE_COUNTER}
