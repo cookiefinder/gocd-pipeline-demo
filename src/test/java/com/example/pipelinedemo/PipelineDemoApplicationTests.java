0006e875511c4e9d0584fe67517a026e3c6eea11
Zijie Jiang <zijie.jiang@thoughtworks.com>
2019-10-16 13:15:10 +0800

    1

--- src/test/java/com/example/pipelinedemo/PipelineDemoApplicationTests.java
+++ src/test/java/com/example/pipelinedemo/PipelineDemoApplicationTests.java
@@ -6,6 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -13,5 +14,10 @@ public class PipelineDemoApplicationTests {
 
 	@Test
 	public void contextLoads() {
+	}
+
+	@Test
+	public void name() {
+		assertTrue(false);
 	}
 }
