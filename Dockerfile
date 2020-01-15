9c0150e4f1264b1bbd8c35397b2f4c6ffbc2ae85
Zijie Jiang <zijie.jiang@thoughtworks.com>
2020-01-15 15:13:38 +0800

    refactor

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,5 @@
 FROM openjdk:8-alpine
-
 MAINTAINER zijie <zijie.jiang@thoughtworks.com>
-
 COPY build/libs/*.jar /app/*.jar
-
 WORKDIR /app
-
 CMD java -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -XX:+PrintGCDateStamps -XX:+PrintHeapAtGC -verbose:gc -XX:+PrintTenuringDistribution -XX:+PrintGCApplicationStoppedTime -Xloggc:gc_cdm.log -jar *.jar
--- scripts/test.sh
+++ scripts/test.sh
@@ -2,4 +2,9 @@
 set -x
 set -e
 
-docker run --rm -v /tmp/gradle:/home/gradle/.gradle -v $WORKDIR/pipelines/$GO_PIPELINE_NAME:/home/gradle -w /home/gradle gradle:4.4-jdk8 ./gradlew clean test
+docker run --rm \
+    -v /tmp/gradle:/home/gradle/.gradle \
+    -v $WORKDIR/pipelines/$GO_PIPELINE_NAME:/home/gradle \
+    -w /home/gradle \
+    gradle:4.4-jdk8 \
+    ./gradlew clean test
